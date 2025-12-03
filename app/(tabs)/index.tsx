ac051839ad56922cd91ee1ad84022ccc1f3521d2
Júlio Andherson <julioandherson1@gmail.com>
2025-12-03 00:31:44 -0300

    adding convex setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,17 @@
+import { api } from "@/convex/_generated/api";
 import { useTheme } from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme();
+
+  // #Just to validade Convex is working
+  // const todos = useQuery(api.todos.getTodos);
+  // console.log("Todos:", todos);
+  // const addTodo = useMutation(api.todos.addTodo);
+  // const deleteTodo = useMutation(api.todos.clearAllTodos);
+
   return (
     <View style={{...styles.container}} >
       <Text>Edit app/index.tsx to edit this screens.</Text>
@@ -10,7 +19,7 @@ export default function Index() {
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle Dark Mode</Text>
       </TouchableOpacity>
-      
+
     </View>
   );
 }
